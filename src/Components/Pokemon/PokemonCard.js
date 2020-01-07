5aceb077f65f44052c469c1c2ccea50242d4b47b
Mizan Choudhury <mizan_98@hotmail.co.uk>
2020-01-07 10:48:59 +0000

    sprite images added

--- src/Components/Pokemon/PokemonCard.js
+++ src/Components/Pokemon/PokemonCard.js
@@ -1,4 +1,12 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Sprite = styled.img `
+    width: 5em
+    height: 5em
+`;
+    
+    
 
 export default class PokemonCard extends Component {
     state ={
@@ -24,6 +32,11 @@ export default class PokemonCard extends Component {
             <div className='col-md-3 col-sm-6 mb-5'>
                 <div className='card'>
                     <h5 className='card-header'>{this.state.pokemonIndex}</h5>
+                    <Sprite className='card-img-top rounded mx-auto mt-2'
+                    src={this.state.imageURL}/>
+
+
+                    
                     <div className='card-body mx-auto'>
                         <h6 className='card-title'>{this.state.name.toLowerCase().split(" ").map(letter => letter.charAt(0).toUpperCase() + letter.substring(1)).join(" ")}</h6>
                     </div>
